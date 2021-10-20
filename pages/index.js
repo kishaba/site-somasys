9f10d3e842c91f2a9a12c7244415b16d2ad6c4f5
Alexandre Mrus <alexandre.azevedo.mrus@gmail.com>
2021-10-20 17:10:52 -0300

    template inicial do novo site somasys
    template inicial do novo site somasys

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,116 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <header className='text-gray-600 body-font'>
+          <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
+            <a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'
+                viewBox='0 0 24 24'
+              >
+                <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5' />
+              </svg>
+              <span className='ml-3 text-xl'>Tailblocks</span>
+            </a>
+            <nav className='md:ml-auto flex flex-wrap items-center text-base justify-center'>
+              <a className='mr-5 hover:text-gray-900'>First Link</a>
+              <a className='mr-5 hover:text-gray-900'>Second Link</a>
+              <a className='mr-5 hover:text-gray-900'>Third Link</a>
+              <a className='mr-5 hover:text-gray-900'>Fourth Link</a>
+            </nav>
+            <button className='inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0'>
+              Button
+              <svg
+                fill='none'
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                className='w-4 h-4 ml-1'
+                viewBox='0 0 24 24'
+              >
+                <path d='M5 12h14M12 5l7 7-7 7' />
+              </svg>
+            </button>
+          </div>
+        </header>
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
+      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
+        <h1 className='text-6xl font-bold'>
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className='text-blue-600' href='https://nextjs.org'>
             Next.js!
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
+        <p className='mt-3 text-2xl'>
           Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+          <code className='p-3 font-mono text-lg bg-gray-100 rounded-md'>pages/index.js</code>
         </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+        <div className='flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full'>
           <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            href='https://nextjs.org/docs'
+            className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600'
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
+            <h3 className='text-2xl font-bold'>Documentation &rarr;</h3>
+            <p className='mt-4 text-xl'>
               Find in-depth information about Next.js features and API.
             </p>
           </a>
 
           <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            href='https://nextjs.org/learn'
+            className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600'
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
+            <h3 className='text-2xl font-bold'>Learn &rarr;</h3>
+            <p className='mt-4 text-xl'>
               Learn about Next.js in an interactive course with quizzes!
             </p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            href='https://github.com/vercel/next.js/tree/master/examples'
+            className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600'
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
+            <h3 className='text-2xl font-bold'>Examples &rarr;</h3>
+            <p className='mt-4 text-xl'>
               Discover and deploy boilerplate example Next.js projects.
             </p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+            className='p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600'
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
+            <h3 className='text-2xl font-bold'>Deploy &rarr;</h3>
+            <p className='mt-4 text-xl'>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className='flex items-center justify-center w-full h-24 border-t'>
         <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='flex items-center justify-center'
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          Powered by <img src='/vercel.svg' alt='Vercel Logo' className='h-4 ml-2' />
         </a>
       </footer>
     </div>
-  )
+  );
 }
