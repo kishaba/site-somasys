3a5c68fb172ccb4eaa451c2854d4662966eabd2a
Alexandre Mrus <alexandre.azevedo.mrus@gmail.com>
2021-11-09 11:24:03 -0300

    Responsividade e componetização do Menu

--- components/Burger/index.jsx
+++ components/Burger/index.jsx
@@ -1,81 +1,138 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { Transition } from '@headlessui/react';
+import { Link } from 'react-scroll';
 
 const Burger = () => {
-    return (
-        <div className='fixed flex flex-col text-white bg-gray-500 px-5 py-5 right-0'>
-            <MenuToggle />
-            <MenuItem>Home</MenuItem>
-            <MenuItem>Sobre</MenuItem>
-            <MenuItem>Blog</MenuItem>
-            <MenuItem>Serviços</MenuItem>
-            <MenuItem>Parceiros</MenuItem>
-            <MenuItem>Contato</MenuItem>
-        </div>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const OpenCloseMenu = () => setIsOpen(!isOpen);
 
-const MenuItem = ({ children }) => {
-    return (
-        <a className='hover:text-green-soma hover:cursor-pointer px-5 py-5 right-0'>
-            {children}
-        </a>
-    );
+  return (
+    <div>
+      <div className='flex justify-end md:hidden z-50 absolute inset-y-0 right-0'>
+        <BurguerButton onClick={OpenCloseMenu}>
+          <span className='sr-only'>Open main menu</span>
+          <BurguerSvG isOpen={isOpen} />
+        </BurguerButton>
+      </div>
+      <BurguerNav isOpen={isOpen}>
+        <div className='md:hidden' id='mobile-menu'>
+          <BuguerLink onClick={OpenCloseMenu}/>
+        </div>
+      </BurguerNav>
+    </div>
+  );
 };
 
-const MenuToggle = ({ children }) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    return (
-        <div className="flex flex-wrap p-5 items-center">
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 
-                  hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-offset-2
-                  focus:ring-offset-gray-800 md:flex-row"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-            >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                    <svg
-                        className="block h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h16M4 18h16"
-                        />
-                    </svg>
-                ) : (
-                    <svg
-                        className="block h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M6 18L18 6M6 6l12 12"
-                        />
-                    </svg>
-                )}
-            </button>
-        </div>
-    );
+const BurguerButton = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      type='button'
+      className='inline-flex items-center justify-center p-2 h-12 w-12 rounded-md text-white  hover:text-white   shadow-xl rounded-xl bg-gradient-to-b from-blue-soma backdrop-blur'
+      aria-controls='mobile-menu'
+      aria-expanded='false'
+    >
+      {children}
+    </button>
+  );
 };
 
-Burger.item = MenuItem;
-Burger.toggle = MenuToggle;
+const BurguerItem = ({ children }) => {
+  return (
+    <div className='hover:scale-110 hover:text-blue-soma hover:cursor-pointer inline'>
+      {children}
+    </div>
+  );
+};
+
+const BuguerLink = ({onClick}) => {
+  return (
+    <div className='flex flex-col px-12 pt-6 h-96 absolute inset-x-0 top-0 z-30 space-y-6  bg-gradient-to-b from-blue-soma backdrop-blur shadow-xl text-2xl '>
+      <Link onClick={onClick} to='home' smooth={true}>
+        <BurguerItem>Home</BurguerItem>
+      </Link>
+
+      <Link onClick={onClick} to='about' smooth={true}>
+        <BurguerItem>Sobre</BurguerItem>
+      </Link>
+
+      <Link onClick={onClick} to='services' smooth={true}>
+        <BurguerItem> Serviços</BurguerItem>
+      </Link>
+
+      <Link onClick={onClick} to='clients' smooth={true}>
+        <BurguerItem>Parceiros</BurguerItem>
+      </Link>
+
+      <Link onClick={onClick} to='contact' smooth={true}>
+        <BurguerItem>Contato</BurguerItem>
+      </Link>
+
+      <a href='/blog'>
+        <BurguerItem>Blog</BurguerItem>
+      </a>
+    </div>
+  );
+};
+
+const BurguerSvG = ({ isOpen }) => {
+  return (
+    <div>
+      {!isOpen ? (
+        <svg
+          className='block h-6 w-6 '
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke='currentColor'
+          aria-hidden='true'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            d='M4 6h16M4 12h16M4 18h16'
+          />
+        </svg>
+      ) : (
+        <svg
+          className='block h-6 w-6'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke='currentColor'
+          aria-hidden='true'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            d='M6 18L18 6M6 6l12 12'
+          />
+        </svg>
+      )}
+    </div>
+  );
+};
+
+const BurguerNav = ({ children, isOpen }) => {
+  return (
+    <Transition
+      show={isOpen}
+      enter='transition ease-out duration-100 transform'
+      enterFrom='opacity-0 scale-95'
+      enterTo='opacity-100 scale-100'
+      leave='transition ease-in duration-75 transform'
+      leaveFrom='opacity-100 scale-100'
+      leaveTo='opacity-0 scale-95'
+    >
+      <div className='md:hidden' id='mobile-menu'>
+        <div className='flex flex-col px-12 pt-6 h-96 absolute inset-x-0 top-0 z-30 space-y-6  bg-gradient-to-b from-blue-soma backdrop-blur shadow-xl text-2xl '>
+          {children}
+        </div>
+      </div>
+    </Transition>
+  );
+};
 
 export default Burger;
--- components/Menu/index.jsx
+++ components/Menu/index.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Transition } from '@headlessui/react';
 import { Link } from 'react-scroll';
+import Burger from '../Burger';
 
-const Menu = () => {
+function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
   const [navbar, setNavbar] = useState(false);
   const changeNavbar = () => {
     if (window.scrollY >= 66) {
@@ -31,18 +34,53 @@ const Menu = () => {
   });
 
   return (
-    <>
-      <div className='fixed w-full z-50'>
-        <div className={colorChange ? 'text-white' : 'text-white'}>
-          <div className={navbar ? 'bg-gradient-to-b from-blue-soma backdrop-blur shadow-xl' : ''}>
-            <div className='container px-5 py-3 mx-auto text-center items-center flex flex-row '>
-              <MenuBrand></MenuBrand>
-              <MenuNav></MenuNav>
+    <div>
+      <nav className='fixed w-full z-50 text-white'>
+        <div className='fixed w-full z-50'>
+          <div
+            className={colorChange ? 'text-white  hidden md:block' : 'text-white  hidden md:block'}
+          >
+            <div
+              className={navbar ? 'bg-gradient-to-b from-blue-soma backdrop-blur shadow-xl' : ''}
+            >
+              <div className='container px-5 py-3 mx-auto text-center items-center flex flex-row '>
+                <MenuBrand></MenuBrand>
+                <MenuNav></MenuNav>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
+
+        <Burger />
+      </nav>
+    </div>
+  );
+}
+
+const MenuItem = ({ children }) => {
+  const [colorChange, setColorchange] = useState(false);
+  const changeBackground = () => {
+    if (window.scrollY >= 600) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+  useEffect(() => {
+    changeBackground();
+
+    window.addEventListener('scroll', changeBackground);
+  });
+  return (
+    <div
+      className={
+        colorChange
+          ? 'hover:scale-110 hover:text-blue-soma hover:cursor-pointer inline'
+          : 'hover:scale-110 hover:text-blue-soma hover:cursor-pointer inline'
+      }
+    >
+      {children}
+    </div>
   );
 };
 
@@ -57,7 +95,6 @@ const MenuBrand = () => {
 const MenuNav = () => {
   return (
     <div className='space-x-4 hidden md:block md:ml-auto items-center text-xl'>
-    
       <Link to='home' smooth={true}>
         <MenuItem>Home</MenuItem>
       </Link>
@@ -78,44 +115,132 @@ const MenuNav = () => {
         <MenuItem>Contato</MenuItem>
       </Link>
 
-      <a href='/blog' >
+      <a href='/blog'>
         <MenuItem>Blog</MenuItem>
       </a>
-
-
     </div>
   );
 };
-
-const MenuItem = ({ children }) => {
-  const [colorChange, setColorchange] = useState(false);
-  const changeBackground = () => {
-    if (window.scrollY >= 600) {
-      setColorchange(true);
-    } else {
-      setColorchange(false);
-    }
-  };
-  useEffect(() => {
-    changeBackground();
-
-    window.addEventListener('scroll', changeBackground);
-  });
-  return (
-    <div 
-      className={
-        colorChange
-          ? 'hover:scale-110 hover:text-blue-soma hover:cursor-pointer inline'
-          : 'hover:scale-110 hover:text-blue-soma hover:cursor-pointer inline'
-      }
-    >
-      {children}
-    </div>
-  );
-};
-
-Menu.Brand = MenuBrand;
-Menu.Nav = MenuNav;
-Menu.item = MenuItem;
 
 export default Menu;
+
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-scroll';
+
+// const Menu = () => {
+//   const [navbar, setNavbar] = useState(false);
+//   const changeNavbar = () => {
+//     if (window.scrollY >= 66) {
+//       setNavbar(true);
+//     } else {
+//       setNavbar(false);
+//     }
+//   };
+//   useEffect(() => {
+//     changeNavbar();
+
+//     window.addEventListener('scroll', changeNavbar);
+//   });
+
+//   const [colorChange, setColorchange] = useState(false);
+//   const changeBackground = () => {
+//     if (window.scrollY >= 600) {
+//       setColorchange(true);
+//     } else {
+//       setColorchange(false);
+//     }
+//   };
+//   useEffect(() => {
+//     changeBackground();
+
+//     window.addEventListener('scroll', changeBackground);
+//   });
+
+//   return (
+//     <>
+//       <div className='fixed w-full z-50'>
+//         <div className={colorChange ? 'text-white' : 'text-white'}>
+//           <div className={navbar ? 'bg-gradient-to-b from-blue-soma backdrop-blur shadow-xl' : ''}>
+//             <div className='container px-5 py-3 mx-auto text-center items-center flex flex-row '>
+//               <MenuBrand></MenuBrand>
+//               <MenuNav></MenuNav>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// const MenuBrand = () => {
+//   return (
+//     <div className='flex-1'>
+//       <img className='object-contain h-14 hidden md:block' alt='logo' src='logo.png' />
+//     </div>
+//   );
+// };
+
+// const MenuNav = () => {
+//   return (
+//     <div className='space-x-4 hidden md:block md:ml-auto items-center text-xl'>
+
+//       <Link to='home' smooth={true}>
+//         <MenuItem>Home</MenuItem>
+//       </Link>
+
+//       <Link to='about' smooth={true}>
+//         <MenuItem>Sobre</MenuItem>
+//       </Link>
+
+//       <Link to='services' smooth={true}>
+//         <MenuItem>Serviços</MenuItem>
+//       </Link>
+
+//       <Link to='clients' smooth={true}>
+//         <MenuItem>Parceiros</MenuItem>
+//       </Link>
+
+//       <Link to='contact' smooth={true}>
+//         <MenuItem>Contato</MenuItem>
+//       </Link>
+
+//       <a href='/blog' >
+//         <MenuItem>Blog</MenuItem>
+//       </a>
+
+//     </div>
+//   );
+// };
+
+// const MenuItem = ({ children }) => {
+//   const [colorChange, setColorchange] = useState(false);
+//   const changeBackground = () => {
+//     if (window.scrollY >= 600) {
+//       setColorchange(true);
+//     } else {
+//       setColorchange(false);
+//     }
+//   };
+//   useEffect(() => {
+//     changeBackground();
+
+//     window.addEventListener('scroll', changeBackground);
+//   });
+//   return (
+//     <div
+//       className={
+//         colorChange
+//           ? 'hover:scale-110 hover:text-blue-soma hover:cursor-pointer inline'
+//           : 'hover:scale-110 hover:text-blue-soma hover:cursor-pointer inline'
+//       }
+//     >
+//       {children}
+//     </div>
+//   );
+// };
+
+// Menu.Brand = MenuBrand;
+// Menu.Nav = MenuNav;
+// Menu.item = MenuItem;
+
+// export default Menu;
