594a2cdb97402f428bfab4b39f6db58eeabc5474
Alexandre Mrus <alexandre.azevedo.mrus@gmail.com>
2021-11-26 15:29:57 -0300

    feature: Added new Slide component, and compnetized Clients

---
+++ src/components/Clients/Slide/index.jsx
@@ -0,0 +1,61 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Slide = ({ children }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+    accessibility: true,
+    nextArrow: <SampleArrow />,
+    prevArrow: <SampleArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          className: 'h-24',
+          accessibility: true,
+          dots: false,
+        },
+      },
+    ],
+  };
+  return <Slider {...settings}>{children}</Slider>;
+};
+
+const SampleArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: '#328381' }}
+      onClick={onClick}
+    />
+  );
+};
+
+export default Slide;
